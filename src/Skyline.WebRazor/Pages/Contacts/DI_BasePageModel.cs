7d97e132b36e7cd42bd5e4b343b16d2b2dd44053
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-17 20:21:23 +0800

    feat: 通讯录首页列表

---
+++ src/Skyline.WebRazor/Pages/Contacts/DI_BasePageModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Skyline.Domain.Identity;
+using Skyline.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skyline.WebRazor.Pages.Contacts
+{
+    public class DI_BasePageModel : PageModel
+    {
+        public ApplicationDbContext Context { get; }
+        protected IAuthorizationService AuthorizationService { get; }
+        protected UserManager<AppUser> UserManager { get; }
+        public DI_BasePageModel(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<AppUser> userManager) : base()
+        {
+            Context = context;
+            UserManager = userManager;
+            AuthorizationService = authorizationService;
+        }
+    }
+}
---
+++ src/Skyline.WebRazor/Pages/Contacts/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Skyline.Domain.ContactAggregate;
+using Skyline.Domain.Identity;
+using Skyline.Infrastructure;
+
+namespace Skyline.WebRazor.Pages.Contacts
+{
+    public class IndexModel : DI_BasePageModel
+    {
+        public IndexModel(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<AppUser> userManager) : base(context, authorizationService, userManager)
+        {
+        }
+        public IList<Contact> Contacts { get; set; }
+        public async Task OnGet()
+        {
+            var contact = from c in Context.Contacts
+                          select c;
+            var currentUserId = UserManager.GetUserId(User);
+            Contacts = await contact.ToListAsync();
+        }
+    }
+}

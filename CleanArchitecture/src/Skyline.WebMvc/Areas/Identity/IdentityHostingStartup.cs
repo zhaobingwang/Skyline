d71caf89b77f343b3df168ed776fc0d440cbcfc5
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-22 00:04:20 +0800

    feat(CleanArchitecture): 添加Asp.Net Core Identity

---
+++ CleanArchitecture/src/Skyline.WebMvc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Skyline.Infrastructure.Identity;
+
+[assembly: HostingStartup(typeof(Skyline.WebMvc.Areas.Identity.IdentityHostingStartup))]
+namespace Skyline.WebMvc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppIdentityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AppIdentityDbContextConnection")));
+
+                services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AppIdentityDbContext>();
+            });
+        }
+    }
+}

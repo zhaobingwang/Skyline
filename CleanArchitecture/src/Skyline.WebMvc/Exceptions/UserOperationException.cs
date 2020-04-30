39de3887abbc510ef7c55934039a55d132e3a36d
zhaobingwang <zhaobingwang@foxmail.com>
2020-04-30 12:46:55 +0800

    feat(CleanArchitecture.WebMvc): 全局异常过滤

---
+++ CleanArchitecture/src/Skyline.WebMvc/Exceptions/UserOperationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skyline.WebMvc.Exceptions
+{
+    public class UserOperationException : Exception
+    {
+        /// <summary>
+        /// 用户操作异常
+        /// </summary>
+        public UserOperationException()
+        {
+
+        }
+        public UserOperationException(string message) : base(message)
+        {
+
+        }
+        public UserOperationException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
